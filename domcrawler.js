4275e82e18980bbf44ff2e10f8fd0b208ceb1a77
Caleb Rogers <rogersjcaleb@gmail.com>
2016-02-18 10:23:07 -0800

    basic functionality established on one page

--- domcrawler.js
+++ domcrawler.js
@@ -1,8 +1,8 @@
 var domcrawler = function(){
   var collection = [];
 
-  collection = $('* img'.map(function(){
-    return {id: $(this).id, src: $(this).prop('src')};
+  collection = $('img').map(function(){
+    return $(this).prop('src');
   }).get();
 
 
